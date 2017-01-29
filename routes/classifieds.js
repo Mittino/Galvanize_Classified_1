a5ea0720e53e2ef165f16ba9937f0504d7692173
mittino <annamefford@gmail.com>
2017-01-29 11:02:30 -0700

    working on first route

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -5,6 +5,17 @@ const express = require('express');
 
 const router = express.Router();
 
-// YOUR CODE HERE
+const knex = require('knex');
+router.use(knex);
+
+router.get('/classifieds', function(req,res,next){
+  knex.select('*').from('classifieds')
+  .then((results) => {
+    res.send(results);
+  }) .catch((err) =>{
+    res.send('error getting classifieds');
+  });
+});
+
 
 module.exports = router;
