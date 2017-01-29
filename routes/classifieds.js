168184246fc55c19aac96049df37c18f9a0e6176
mittino <annamefford@gmail.com>
2017-01-29 11:32:31 -0700

    first get route test passing

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -5,12 +5,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const knex = require('knex');
-router.use(knex);
+const knex = require('../knex');
+//router.use(knex);
 
-router.get('/classifieds', function(req,res,next){
-  knex.select('*').from('classifieds')
+router.get('/', function(req,res,next){
+  knex('classifieds').select('id', 'title', 'description', 'price', 'item_image')
   .then((results) => {
+    console.log(results);
     res.send(results);
   }) .catch((err) =>{
     res.send('error getting classifieds');
