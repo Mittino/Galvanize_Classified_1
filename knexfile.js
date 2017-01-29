c84a88b5085ed7c2b648dd32fb1519a01e0d0fe2
mittino <annamefford@gmail.com>
2017-01-29 12:49:52 -0700

    working on pg db on heroku

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,10 @@ module.exports = {
     database: 'gclassifieds_test',
     connection:'postgres://localhost/gclassifieds_test'
   },
-  production: { },
+  production: {
+    client:'pg',
+    connection:'postgres://jvgfcfdehabbzz:ca4d72b3b4c2dda38ac971fc47565ac3cb210baffb01bdd32f31b7ff426ac1f3@ec2-107-20-195-181.compute-1.amazonaws.com:5432/dedga3qeteu4d7'
+
+},
 
 };
